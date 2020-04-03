471e7fb2cf7fb7639fc56bd70e4d1e97567ac310
thiagosilvadevpr <thiagosilvadevpr@gmail.com>
2020-04-02 22:26:21 -0300

    Alteração na Classe Main do Relatório ZooMax

---
+++ CLIENTES/ZOOMAX/RELATORIOS/OrdemMateriaisProducao/Main.pas
@@ -0,0 +1,32 @@
+procedure BitBtn1OnClick(Sender: TfrxComponent);
+begin
+    qryProdutos.SQL.ADD(
+        ' AND MOV.CODIGO IN ' + VarToStr(<CODIGOS_MOVIMENTO>)
+    );
+    qryProdutos.SQL.ADD(
+        ' GROUP BY '
+        + ' ITM.IDPRODUTO,  '
+        + ' ITM.DESCPRODUTO '
+    );
+    //ShowMessage(qryProdutos.SQL.Text);
+end;
+
+procedure MasterData1OnBeforePrint(Sender: TfrxComponent);
+begin
+    if Frac(<Line> / 2) = 0.5 then
+    begin              
+        mmIdProduto.Color := $00EBEBEB;        
+        mmDescricao.Color := $00EBEBEB;
+        mmQuantidade.Color := $00EBEBEB;        
+    end            
+    else
+        begin              
+            mmIdProduto.Color := clWhite;
+            mmDescricao.Color := clWhite;
+            mmQuantidade.Color := clWhite;
+        end;         
+end;
+
+begin
+
+end.
